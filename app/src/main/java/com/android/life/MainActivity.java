4279209e21b33ecc6a648a1411e5fb63eed068ce
nagubabu <ch.nagubabu@gmail.com>
2015-05-29 16:14:55 +0530

    Login Activity

--- app/src/main/java/com/android/life/MainActivity.java
+++ app/src/main/java/com/android/life/MainActivity.java
@@ -1,17 +1,25 @@
 package com.android.life;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements View.OnClickListener {
+
+    Button btn_login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_login = (Button) findViewById(R.id.btn_login);
+        btn_login.setOnClickListener(this);
     }
 
     @Override
@@ -34,5 +42,13 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == btn_login){
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
+        }
     }
 }
